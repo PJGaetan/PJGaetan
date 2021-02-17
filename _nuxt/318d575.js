7637ecf17fe89ea184e67e11f9815dee4e7f77e0
PJGaetan <43468407+PJGaetan@users.noreply.github.com>
2021-02-17 22:37:37 +0000

    Deploying to gh-pages from @ PJGaetan/mywebsite@0acae933bf4c8cd8937bbb8fcac4ff5f423e660f 🚀

---
+++ _nuxt/318d575.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{233:function(t,n,e){"use strict";e.r(n);var c=e(25),component=Object(c.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"},[this._v("About")])}),[],!1,null,null,null);n.default=component.exports}}]);
