57e12b4779c585ffe35fa4b35deaa7341fc64a7e
PJGaetan <43468407+PJGaetan@users.noreply.github.com>
2021-04-12 22:57:51 +0000

    Deploying to gh-pages from @ PJGaetan/mywebsite@f9930ddea37ed0e1a328d22ab2618e9bcff606d2 🚀

---
+++ _nuxt/static/1618268260/PJGaetan/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618268260",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"bce2f7e4"}}}}("\u002FPJGaetan"));
